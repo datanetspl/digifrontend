7e50a8c0f3db0161eb6496fd9c4942648f84aee6
utkala mohanty <utkala@gmail.com>
2023-06-21 21:20:35 +0800

    changes

--- src/env.js
+++ src/env.js
@@ -1,6 +1,7 @@
 export default
     {
-        "API_URL": "https://localhost", // Address for backend api url
+        //"API_URL": "https://localhost", // Address for backend api url
+        "API_URL": "https://digimarketwebapi20230621143456.azurewebsites.net/",
         "PORT": 44300, // port of backend api 
         "SOCKET": "http://127.0.0.1:4001", // socket url
         "JITSI": "meet.jit.si" // jitsi cloud server url
